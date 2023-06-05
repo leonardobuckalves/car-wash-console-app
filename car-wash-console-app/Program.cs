49ac6e74dfb7bbd2ddebe9efb715739c722b29a4
leonardobalves <leonardobuckalves@gmail.com>
2023-06-05 19:19:29 -0300

    Add project files.

---
+++ car-wash-console-app/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace car_wash_console_app
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
